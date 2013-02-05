e79bebabefcec7c29a47db4f17ab8eb6ef1106b2
Marcus Westin <marcus.westin@gmail.com>
2013-02-05 10:58:23 -0800

    Add array.toObject

--- js-HandyHelpers/Array+Util.js
+++ js-HandyHelpers/Array+Util.js
@@ -7,4 +7,10 @@
 		return this.slice(0, index).concat(this.slice(index+1, this.length))
 	}
 	
+	array.toObject = function() {
+		var obj = {}
+		for (var i=0; i<this.length; i++) { obj[this[i]] = true }
+		return obj
+	}
+	
 }(Array.prototype))
